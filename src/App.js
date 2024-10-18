36ba24ea128cc22f8e6e5cb72dd641a193913793
jamesjo <jamescakr@gmail.com>
2024-10-18 10:26:27 -0600

    2nd assignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import api from "./utils/api";
 
 import TodoBoard from "./components/TodoBoard";
 
@@ -8,22 +10,58 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  const getTasks = async () => {
+    const response = await api.get("/tasks");
+    console.log("rrr", response);
+    setTodoList(response.data.data);
+  };
+
+  const addTask = async () => {
+    try {
+      const response = await api.post("/tasks", {
+        task: todoValue,
+        isComplete: false,
+      });
+      if(response.status===200){
+        console.log("success!!")
+        setTodoValue("")
+        getTasks()
+      } else {
+        throw new Error("task can not be added")
+      }
+    } catch (err) {
+      console.log("error", err);
+      
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
     <Container>
       <Row className="add-item-row">
         <Col xs={12} sm={10}>
           <input
             type="text"
-            placeholder="할일을 입력하세요"
+            placeholder="Add your tasks"
             className="input-box"
+            value={todoValue}
+            onChange={(event) => setTodoValue(event.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>
+            Add
+          </button>
         </Col>
       </Row>
 
-      <TodoBoard />
+      <TodoBoard todoList={todoList} getTasks={getTasks}/>
     </Container>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,57 @@
-import React from "react";
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import api from "../utils/api";
+import "../App.css";
 
-const TodoItem = () => {
+const TodoItem = ({ item, getTasks }) => {
+  const [deleteItem, setDeleteItem] = useState("");
+  const [isComplete, setIsComplete] = useState(item.isComplete);
+
+  const deleteTask = async () => {
+    try {
+      const response = await api.delete(`/tasks/${item._id}`);
+      if (response.status === 200) {
+        console.log("deleted!!");
+        setDeleteItem("");
+        getTasks();
+      } else {
+        throw new Error("task can not be deleted");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
+  const updateTask = async () => {
+    try {
+      const response = await api.put(`tasks/${item._id}`, {
+        isComplete: !isComplete,
+      });
+      if (response.status === 200) {
+        console.log("updated!!");
+        setIsComplete(!isComplete);
+        getTasks();
+      } else {
+        throw new Error("task can not be updated");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+
   return (
     <Row>
       <Col xs={12}>
         <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+          <div className={`todo-content ${isComplete ? "completed" : ""}`}>{item.task}</div>
 
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button className="button-delete" onClick={deleteTask}>
+              Delete
+            </button>
+            <button className="button-delete" onClick={updateTask}>
+              {isComplete ? "Undo" : "Done"}
+            </button>
           </div>
         </div>
       </Col>
